fc6c670bcf9c18bd81477893ba03437d7d393955
yd072 <luiquirong@gmail.com>
2024-12-16 00:55:58 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import re
 
 # 目标URL列表
-urls = ['https://cf.090227.xyz', 'https://ip.164746.xyz']
+urls = ['https://cf.090227.xyz', 'https://ip.164746.xyz','https://stock.hostmonit.com/CloudFlareYes']
 
 # 正则表达式匹配IP地址
 ip_pattern = r'\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b'
