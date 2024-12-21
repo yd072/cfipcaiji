be1c27bb52e94e963fa5c7a2b971464067ab2717
yd072 <luiquirong@gmail.com>
2024-12-22 07:42:27 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -1,45 +1,26 @@
-import requests
-from bs4 import BeautifulSoup
-import re
-import os
+import socket
 
-# 目标URL列表
-urls = ['https://ip.164746.xyz', 
-        'https://api.uouin.com/cloudflare.html'
-        
-        ]
+# 目标域名列表
+domains = [
+    'proxyip.fxxk.dedyn.io'
+  
+]
 
-# 正则表达式用于匹配IP地址
-ip_pattern = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
-
-# 检查ip.txt文件是否存在,如果存在则删除它
+# 检查ip.txt文件是否存在，如果存在则删除它
 if os.path.exists('ip.txt'):
     os.remove('ip.txt')
 
-# 创建一个文件来存储IP地址
+# 创建一个文件来存储域名及其IP地址
 with open('ip.txt', 'w') as file:
-    for url in urls:
-        # 发送HTTP请求获取网页内容
-        response = requests.get(url)
-        
-        # 使用BeautifulSoup解析HTML
-        soup = BeautifulSoup(response.text, 'html.parser')
-        
-        # 根据网站的不同结构找到包含IP地址的元素
-        if url == 'https://ip.164746.xyz':
-            elements = soup.find_all('tr')
-        elif url == 'https://api.uouin.com/cloudflare.html':
-            elements = soup.find_all('tr')
-        else:
-            elements = soup.find_all('li')
-        
-        # 遍历所有元素,查找IP地址
-        for element in elements:
-            element_text = element.get_text()
-            ip_matches = re.findall(ip_pattern, element_text)
-            
-            # 如果找到IP地址,则写入文件
-            for ip in ip_matches:
-                file.write(ip + '\n')
+    for domain in domains:
+        try:
+            # 使用socket获取IP地址
+            ip_address = socket.gethostbyname(domain)
+            file.write(f'{domain}: {ip_address}\n')
+            print(f'{domain}: {ip_address}')
+        except socket.gaierror:
+            # 如果解析失败，记录错误信息
+            file.write(f'{domain}: Failed to resolve\n')
+            print(f'{domain}: Failed to resolve')
 
-print('IP地址已保存到ip.txt文件中。')
+print('域名解析的IP地址已保存到ip.txt文件中。')
